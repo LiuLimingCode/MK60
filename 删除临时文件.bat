7ebb3985b055c5c24455cad4de1254c5e0756f8b
刘力铭 <1104257233@qq.com>
2020-01-05 15:25:51 +0800

    add .gitignore

--- 删除临时文件.bat
+++ 删除临时文件.bat
@@ -1,5 +1,5 @@
 ::@echo off 
 
-@for /r  %%c in (*.crf, *.map, *.lst, *.d, *.o, *.axf, *.htm, *.sct, *.lnp, *.hex, *.dep, *.__i, *.scvd, *.bak) do del "%%c"
+@for /r  %%c in (*.crf, *.map, *.lst, *.d, *.o, *.axf, *.htm, *.sct, *.lnp, *.hex, *.dep, *.__i, *.scvd, *.bak, *.iex) do del "%%c"
 
 pause
